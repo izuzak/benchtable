367dabcf35314657ca1e0f2d5f5b5a75ab1c1755
Ivan Zuzak <izuzak@gmail.com>
2013-01-01 12:08:22 +0100

    make code a bit more readable

--- benchtable.js
+++ benchtable.js
@@ -11,98 +11,117 @@ var Table = require('cli-table');
 
 function BenchTable(name, options) {
   var self = this;
-  
+
   Benchmark.Suite.call(self, name, options);
-  
+
   self._functions = [];
   self._functionNames = [];
   self._inputs = [];
   self._inputNames = [];
+
+  // mappings from benchmark names to function and input indices
+  // as { funcIdx, inputIdx }
   self._mappings = {};
+
   self._results = {};
-  
+
   /**
-   * Use the 'cycle' event to store results and build a table 
+   * Use the 'cycle' event to store results and build a table
    * after all BenchTable functions are evaluated for all inputs.
    **/
-    
+
   self.on('cycle', function(event) {
     var key = event.target.name;
     var self = this;
-    
+    var worst_idx, best_idx, worst, best, curr;
+    var i, j;
+    var funName, item;
+
     if (!(key in self._mappings)) {
       return;
     }
-    
-    self._results[self._functionNames[self._mappings[key].funcIdx]].push(event.target);
+
+    // store current result
+    funName = self._functionNames[self._mappings[key].funcIdx];
+    self._results[funName].push(event.target);
     self._counter -= 1;
-    
-    if (self._counter === 0) {
-      for (var i=0; i<self._inputs.length; i++) {
-        var worst_idx = 0;
-        var best_idx = 0;
-        
-        for (var j=0; j<self._functions.length; j++) {          
-          if (self._results[self._functionNames[j]][i].hz <= self._results[self._functionNames[worst_idx]][i].hz) {
-            worst_idx = j;
+
+    if (self._counter !== 0) {
+      return;
+    }
+
+    // compute best and worst results for each input
+    for (i=0; i<self._inputs.length; i++) {
+      worst_idx = 0, best_idx = 0;
+      best = self._results[self._functionNames[best_idx]][i];
+      worst = self._results[self._functionNames[worst_idx]][i];
+
+      for (j=0; j<self._functions.length; j++) {
+        curr = self._results[self._functionNames[j]][i];
+
+        if (curr.hz <= worst.hz) {
+          worst_idx = j;
+          worst = self._results[self._functionNames[worst_idx]][i];
+        }
+
+        if (curr.hz >= worst.hz) {
+          best_idx = j;
+          best = self._results[self._functionNames[best_idx]][i];
+        }
+      }
+
+      worst._isWorst = true;
+      best._isBest = true;
+    }
+
+    // create cli-table based on results
+    self.table = new Table({
+      head: [""].concat(self._inputNames),
+       chars: {
+         'top': '-',
+         'top-mid': '+',
+         'top-left': '+',
+         'top-right': '+',
+         'bottom': '-',
+         'bottom-mid': '+',
+         'bottom-left': '+',
+         'bottom-right': '+',
+         'left': '|',
+         'left-mid': '+',
+         'mid': '-',
+         'mid-mid': '+',
+         'right': '|',
+         'right-mid': '+'
+       },
+       truncate: '…'
+    });
+
+    function toTableStr(par) {
+      if (par.error) {
+        return 'ERROR';
+      } else {
+        return Benchmark.formatNumber(par.hz.toFixed(par.hz < 100 ? 2 : 0)) + ' ops/sec';
+      }
+    }
+
+    for (i=0; i<self._functions.length; i++) {
+      item = {};
+      funName = self._functionNames[i];
+      item[funName] = [];
+
+      for (j=0; j<self._results[funName].length; j++) {
+        item[funName].push(toTableStr(self._results[funName][j]));
+
+        if (self._functions.length > 1) {
+          if (self._results[funName][j]._isWorst) {
+            item[funName][j] = item[funName][j].red;
+          } else if (self._results[funName][j]._isBest) {
+            item[funName][j] = item[funName][j].green;
           }
-          
-          if (self._results[self._functionNames[j]][i].hz >= self._results[self._functionNames[best_idx]][i].hz) {
-            best_idx = j;
-          }          
-        }
-                
-        self._results[self._functionNames[worst_idx]][i]._isWorst = true;
-        self._results[self._functionNames[best_idx]][i]._isBest = true;
-      }
-      
-      self.table = new Table({
-        head: [""].concat(self._inputNames),
-         chars: {
-           'top': '-',
-           'top-mid': '+',
-           'top-left': '+',
-           'top-right': '+',
-           'bottom': '-',
-           'bottom-mid': '+',
-           'bottom-left': '+',
-           'bottom-right': '+',
-           'left': '|',
-           'left-mid': '+',
-           'mid': '-',
-           'mid-mid': '+',
-           'right': '|',
-           'right-mid': '+'
-         },
-         truncate: '~'
-      });
-      
-      function toTableStr(par) {
-        if (par.error) {
-          return 'ERROR';
-        } else {
-          return Benchmark.formatNumber(par.hz.toFixed(par.hz < 100 ? 2 : 0)) + ' ops/sec';
         }
       }
-      
-      for (var i=0; i<self._functions.length; i++) {
-        var item = {};
-        item[self._functionNames[i]] = [];
-        
-        for (var j=0; j<self._results[self._functionNames[i]].length; j++) {
-          item[self._functionNames[i]].push(toTableStr(self._results[self._functionNames[i]][j]));
-          
-          if (self._functions.length > 1) {
-            if (self._results[self._functionNames[i]][j]._isWorst) {
-              item[self._functionNames[i]][j] = item[self._functionNames[i]][j].red;
-            } else if (self._results[self._functionNames[i]][j]._isBest) {
-              item[self._functionNames[i]][j] = item[self._functionNames[i]][j].green;
-            }
-          }
-        }
-        
-        self.table.push(item);
-      }
+
+      self.table.push(item);
     }
   });
 }
@@ -117,18 +136,26 @@ BenchTable.prototype = Object.create(Benchmark.Suite.prototype);
  * Wrap Benchmark.Suite.run to additionally create and run all functions for all inputs.
  */
 
-BenchTable.prototype.run = function(config) {  
-  var self = this;
+BenchTable.prototype.run = function(config) {
+  var self = this, mapkey, mapfun;
+
+  function createBenchmarkFunction(fun, input) {
+    return function() {
+      fun.apply(self, input);
+    };
+  }
+
   for (var i=0; i<self._functions.length; i++) {
-    for (var j=0; j<self._inputs.length; j++) {      
-      var mapkey = self._functionNames[i] + " for params " + self._inputNames[j];
-      self.add(mapkey, function(x,y) { return function() { self._functions[x].apply(self, self._inputs[y]); } }(i, j));
-      self._mappings[mapkey] = { funcIdx : i, paramsIdx : j };
+    for (var j=0; j<self._inputs.length; j++) {
+      mapkey = self._functionNames[i] + " for inputs " + self._inputNames[j];
+      mapfun = createBenchmarkFunction(self._functions[i], self._inputs[j]);
+      self.add(mapkey, mapfun);
+      self._mappings[mapkey] = { funcIdx : i, inputIdx : j };
     }
   }
-  
+
   self._counter = self._functions.length * self._inputs.length;
-  
+
   Benchmark.Suite.prototype.run.call(self, config);
 };
 
@@ -140,7 +167,7 @@ BenchTable.prototype.addFunction = function(name, fun) {
   this._functions.push(fun);
   this._functionNames.push(name);
   this._results[name] = [];
-  
+
   return this;
 };
 
@@ -151,7 +178,7 @@ BenchTable.prototype.addFunction = function(name, fun) {
 BenchTable.prototype.addInput = function(name, input) {
   this._inputs.push(input);
   this._inputNames.push(name);
-  
+
   return this;
 };
 
